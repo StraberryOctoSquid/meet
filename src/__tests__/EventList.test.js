5c4d907b83535b53733e2cc4b804e64241de3b04
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-09-12 14:00:17 -0500

    Update EventList.test.js

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/no-render-in-setup */
 /* eslint-disable testing-library/prefer-presence-queries */
 /* eslint-disable testing-library/prefer-screen-queries */
 /* eslint-disable testing-library/render-result-naming-convention */
@@ -6,13 +7,17 @@ import { render } from '@testing-library/react';
 import EventList from '../components/EventList';
 
 describe('<EventList /> component', () => {
+  let EventListComponent;
+  beforeEach(() => {
+    EventListComponent = render(<EventList />);
+  });
+
   test('has an element with "list" as role', () => {
-    const EventListComponent = render(<EventList />);
     expect(EventListComponent.queryByRole('list')).toBeInTheDocument();
 
   });
   test('renders correct number of events', () => {
-    const EventListComponent = render(<EventList events={
+    EventListComponent = render(<EventList events={
       [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
     } />);
     expect(EventListComponent.getAllByRole('listitem')).toHaveLength(4);
