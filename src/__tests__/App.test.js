ecdedb9b2922102c03fc9fe211800ce98e6f3a71
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-09-12 11:18:24 -0500

    Update App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,14 +3,15 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 describe('<App /> component', () => {
+
   test('renders list of events', () => {
     const AppDOM = render(<App />).container.firstChild;
     expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
   });
-  test('renders CitySearch', () => {
-    const AppDOM = render().container.firstChild;
-    expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
 
+  test('renders CitySearch', () => {
+    const AppDOM = render(<App />).container.firstChild;
+    expect(AppDOM.querySelector("#city-search")).toBeInTheDocument();
   });
 }
 );
