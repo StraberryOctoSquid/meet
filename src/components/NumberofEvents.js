360cee85932bea9b7824adedb9d3620dcbfac8e9
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-09-15 07:43:56 -0500

    Create Text box ternary

--- src/components/NumberofEvents.js
+++ src/components/NumberofEvents.js
@@ -2,7 +2,11 @@ const NumberofEvents = ({ setCurrentNOE, setErrorAlert }) => {
 
   const handleInputChanged = (event) => {
     const value = event.target.value;
-    value < 1 ? setErrorAlert('Please enter a number greater than 0') : setErrorAlert('');
+
+    value(isNaN) ? setErrorAlert('Please enter a number between 1 and 50') : setCurrentNOE(value);
+    value > 50 ? setErrorAlert('Please enter a number between 1 and 50') : setCurrentNOE(value);
+    value <= 1 ? setErrorAlert('Please enter a number between 1 and 50') : setCurrentNOE(value);
+    value === '' ? setErrorAlert('Please enter a number between 1 and 50') : setCurrentNOE(value);
   };
   return (
     <div className="NumberofEvents">
@@ -11,8 +15,11 @@ const NumberofEvents = ({ setCurrentNOE, setErrorAlert }) => {
         type="text"
         className="NumberofEventsDisplayed"
         placeholder="Number of Events Displayed"
+        defaultValue={32}
         onChange={handleInputChanged}
       />
     </div>
   );
 };
+
+export default NumberofEvents;
