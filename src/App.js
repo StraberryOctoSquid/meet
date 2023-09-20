adf2843cee3bf733ba587a92c1babd931c4ae552
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-09-20 06:03:01 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberofEvents from './components/NumberofEvents';
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  }
+
   return (
     <div className="App">
       <CitySearch />
       <NumberofEvents />
-      <EventList />
+      <EventList events={events} />
     </div>
   );
 }
